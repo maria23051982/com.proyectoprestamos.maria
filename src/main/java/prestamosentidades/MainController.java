168d66c44c102949d06dd6cbe2fa949eee311f4b
maria23051982 <77923900+maria23051982@users.noreply.github.com>
2021-03-28 10:47:42 -0300

    thymeleaf 28-03 10:47

---
+++ src/main/java/prestamosentidades/MainController.java
@@ -0,0 +1,19 @@
+package prestamosentidades;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	
+	public String welcome(Model model) {
+		
+		model.addAttribute("nombre", "Proyecto.Prestamos");
+		return "index";
+	}
+	
+
+}
