ecea8559cdc7ce5dabe3a6d205b0ac196fe6cf25
maria23051982 <77923900+maria23051982@users.noreply.github.com>
2021-04-09 06:28:08 -0300

    commit 09-04 06:27

--- src/main/java/com/example/demo/controller/PersonaController.java
+++ src/main/java/com/example/demo/controller/PersonaController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
 
-
+import java.text.ParseException;
 import java.util.Optional;
-
 import javax.validation.constraints.Email;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,31 +12,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
 import com.example.demo.prestamosentidades.Persona;
-
 import com.example.demo.repositorios.PersonaRepository;
 
 @Controller
 @RequestMapping("/personas")
 public class PersonaController {
 
+	
+	
 	@Autowired
 	PersonaRepository personaRepository;
+	private Email email;
 
-	@RequestMapping("/")
+	@RequestMapping("/alta")
 	public String alta(Model model) {
-		model.addAttribute("personas", new Persona());
 		Persona persona = new Persona();
-		return "personas/alta";
-		
+		model.addAttribute("personas", persona);
+
+		return "alta";
 	}
 
 	@RequestMapping("/listado")
 	public String list(Model model) {
 		model.addAttribute("personas", personaRepository.findAll());
-		return "personas/listado";
+		return "listado";
 	}
 
 	@RequestMapping(value = "/personas", method = { RequestMethod.POST, RequestMethod.PUT })
@@ -51,7 +49,6 @@ public class PersonaController {
 
 		model.addAttribute("personas", persona.get());
 		return "editar";
-		
 	}
 
 	@PostMapping("/save")
@@ -62,46 +59,48 @@ public class PersonaController {
 			@RequestParam(value = "email", required = true) Email email,
 			@RequestParam(value = "password", required = true) String password)
 			{
-		
-		
-		Persona persona = personaRepository.findByDni(dni).get();
-		if (persona == null)
-			return "redirect:/error";
 
-		
-		persona.setDni(dni);
-		persona.setNombre(nombre);
-		persona.setApellido(apellido);
-		persona.setDtype(dtype);
-		persona.setEmail(email);
-		persona.setPassword(password);
-		System.out.println(persona);
-		
-		personaRepository.save(persona);
+
+      this.setEmail(email);
+		return "redirect:/listado";
+	}
+
+	@RequestMapping(value = "/edit/{id}", method = { RequestMethod.POST, RequestMethod.PUT })
+	public String edit(@PathVariable(value = "id") Integer id,
+			@RequestParam(value = "dni", required = true) Long dni,
+			@RequestParam(value = "dtype", required = true) String dtype,
+			@RequestParam(value = "nombre", required = true) String nombre,
+			@RequestParam(value = "apellido", required = true) String apellido,
+			@RequestParam(value = "email", required = true) Email email,
+			@RequestParam(value = "password", required = true) String password,
+			@RequestParam(value = "activo", required = false) Boolean activo, Model model) throws ParseException {
+
 
 		return "redirect:/listado";
 	}
 
 	@PostMapping(value = "/conf-borrar")
 	public String borrar(@RequestParam(value = "id") int id, Model model) {
-//		Persona persona = usuarioRepository.findById(id);
-//		model.addAttribute("persona", persona);
+
 		return "confirmarBorrado";
 	}
 
 	@PostMapping(value = "/borrar/{id}")
 	public String personaBorrada(@PathVariable(value = "id") int id, Model model) {
-//		try {
-//			usuarioRepository.delete(id);
-//		} catch (Exception e) {
-//			model.addAttribute("error", "No se pudo eliminar el registro");
-//			return "error";
-//		}
+
 		return "personaBorrada";
 	}
 
 	@GetMapping(value = "/error")
 	public String error(Model model) {
 		return "error";
+	}
+
+	public Email getEmail() {
+		return email;
+	}
+
+	public void setEmail(Email email) {
+		this.email = email;
 	}
 }
